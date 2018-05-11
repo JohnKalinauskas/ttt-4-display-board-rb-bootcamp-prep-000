254d08df4358c62c1db599aa430b522e0b22745a
John Kalinauskas <jkalinauskas@live.com>
2018-05-11 20:03:11 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,14 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
+<<<<<<< HEAD
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+=======
+
+>>>>>>> 8e4e5c4712c9a1eb6a9b94fefbeb59a3c961950f
   
 end
 
